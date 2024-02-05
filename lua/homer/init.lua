1303812b701fc1f478fa0499475a1f2ecf548d7c
Subash Katel <subashkatel.sk@gmail.com>
2024-02-04 16:17:32 -0800

    updated

--- lua/homer/init.lua
+++ lua/homer/init.lua
@@ -1,2 +1,12 @@
 require("homer.set")
 require("homer.remap")
+
+require("homer.lazy_init")
+
+-- DO.not
+-- DO NOT INCLUDE THIS
+
+-- If i want to keep doing lsp debugging
+-- function restart_htmx_lsp()
+--     require("lsp-debug-tools").restart({ expected = {}, name = "htmx-lsp", cmd = { "htmx-lsp", "--level", "DEBUG" }, root_dir = vim.loop.cwd(), });
+-- end
